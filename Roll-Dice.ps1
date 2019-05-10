e0c3b50ced14649d4ecd0ef4a7d0ecbf65819d1d
eliteclass13 <eliteclass13@gmail.com>
2019-05-10 12:39:21 -0700

    Update Roll-Dice.ps1
    Added get-help information

--- Roll-Dice.ps1
+++ Roll-Dice.ps1
@@ -1,7 +1,62 @@
 Function Roll-Dice{   
     <#
-    .SYNOPSIS
-        Makes dice roll based on number of dice rolled and value of the individual dice. A bonus may be added to the roll.
+.SYNOPSIS
+	Roll-Dice by default will roll 1 d20 value of dice and output the result as int32 DataType. 
+    The parameters that can be altered are the number of dice rolled, the value of the dice rolled and the stat bonus added after the roll.
+.DESCRIPTION
+	A dice rolling function optimized for D7D 5e gameplay that has been tested with over a million "Throws" to verify the ods are similar to regular physical dice.
+.PARAMETER 
+NumberDice
+    The number of dice that are being "Thrown"
+.PARAMETER 
+DiceValue
+	The maximum number that can be rolled on the dice that are being "Thrown", Currently all dice thrown can only be one value
+.PARAMETER 
+Bonus
+    The bonus from Stats, Proficiency ect... this gets added to the roll once all dice are "Thrown"
+.PARAMETER 
+Advantage
+    A type of roll that comes from D&D 5e that allows 2 dice throwns and the higher result is taken.
+    This cmdlet will make 2 rolls and choose the better result if advantage is chosen
+.PARAMETER 
+Disadvantage
+    A type of roll from D&D 5e that is the opposite of Advantage. Where after 2 rolls the worse result is taken
+.PARAMETER 
+Critical
+    A roll type in D&D 5e usually from damage dice, where the value of the dice is doubled before the bonus is added, creating a large reuslt
+    
+.EXAMPLE
+Roll-Dice 
+    This will simulate one roll of a D20 dice with no Advantage or Disadvantage
+.EXAMPLE
+Roll-Dice 10 6 40
+	This will Simulate ten rolls using a D6 and add a bonus of 40 to the reult, like the damage from the spell Disintegrate
+.EXAMPLE
+Roll-Dice -Advantage
+    This will roll a D20 dice twice with no bonus and take the higher result
+.EXAMPLE
+Roll-Dice -Disadvantage
+    This will roll a D20 dice twice with no bonus and take the lower result
+.EXAMPLE
+Roll-Dice -Disadvantage -Bonus 16
+    This will roll as before with Disadvantage but will add the attack bonus of 16 to each roll and will return with the lower roll
+.EXAMPLE
+Roll-Dice 2 8 10 -Critical
+    This is after a critical hit is struck it will roll weapon damage of 2 d8 that get doubled with a bonus of 10 that isn't doubled.
+.EXAMPLE
+Roll-Dice -DiceValue 8 -Bonus 3 -NumberDice 2 -Bonus 3
+    This will roll 2 d8s with a bonus of 3
+.EXAMPLE
+Roll-Dice -verbose
+The Verbose tag will show the result of every dice "Throw" and every total result
+.INPUTS
+	[int32]$NumberDice, [int32]$DiceValue, [int32]$Bonus, [Switch]$Advantage, [Switch]$Disadvantage, [Switch]$Normal, [Switch]$Critical
+.OUTPUTS
+	PSObject with properties: Name,Version,Computer,UninstallCommand
+.NOTES
+	Author: Daniel Monteleone
+.LINK
+https://github.com/eliteclass13/Roll-Dice/edit/master/Roll-Dice.ps1
     #>
 
   [CmdletBinding(DefaultParameterSetName = 'Normal')]
@@ -34,11 +89,6 @@ Function Roll-Dice{
    [Parameter(ParameterSetName = 'Advantage')]
    [Parameter(ParameterSetName = 'Disadvantage')]
    [Switch]$Critical
-<#
-   [Parameter(Mandatory = $false)]
-   [ValidateSet("Advantage", "None", "Disadvantage")]
-   [String[]]$Advantage = "None"
-#>
     
    )
 
@@ -74,7 +124,6 @@ Function Roll-Dice{
         
         return $output
     }
-    #Start-sleep -Seconds 2
 
     if($PSCmdlet.ParameterSetName -eq 'Normal'){Roll}
 
